731d3aa0268fb6618106940ce46de337a8fb0cf3
Gavi Baron <barongavriel@gmail.com>
2020-02-26 18:20:00 -0500

    dark mode added

--- nhei.js
+++ nhei.js
@@ -1,29 +1,6 @@
-function get_information(link, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", link, true);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4) {
-      callback(xhr.responseText);
-    }
-  };
-  xhr.send(null);
+function switchTheme() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
 }
-get_information("/hall_of_fame.html", function() {
-    var div = document.getElementById("hof");
-    div.innerHTML = text;
-    // Do something with the div here, like inserting it into the page
-});
-
-
-function modifyContext(context) {
-  const div = document.getElementById("hof");
-  div.innerHTML = "Congradulations "
-  context.movieName = movieName;
-  var ratingName = "rotten";
-	if(context.rating >= 60){
-    ratingName = "fresh";
-	}
-	context.ratingName = ratingName;
-  return context;
-}
-window.addEventListener("load", renderTemplate);
+const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
+window.addEventListener('change', switchTheme, false)''
