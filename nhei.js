772cf33001c92a6a1dc42dcb83bb2c888ef52b1a
Gavi Baron <barongavriel@gmail.com>
2020-06-26 14:45:29 -0400

    adding js

--- nhei.js
+++ nhei.js
@@ -1,6 +1,45 @@
-function switchTheme() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
+{
+  let xhr;
+  function getJSONasync(url, handler, errorHandler) {
+	xhr = new XMLHttpRequest();
+	xhr.addEventListener("load", handler);
+	xhr.addEventListener("error", errorHandler);
+	if (!xhr) {
+	  alert('Giving up :( Cannot create an XMLHTTP instance');
+	  return false;
+	}
+	xhr.open('GET', url, true);
+	xhr.send();
+  }
+  
+  function showContents() {
+	const context = modifyContext(JSON.parse(xhr.responseText));
+	const template = document.querySelector("#pr_template").innerHTML;
+	const templateScript = Handlebars.compile(template);
+	const html = templateScript(context);
+	document.querySelector("body").innerHTML = html; 
+  }
+
+  function loadTemplate() {
+  	const hbtext = xhr.responseText;
+  	document.querySelector("#pr_template").innerHTML = hbtext;
+    const contextName = "power_rankings.json"; 
+  	getJSONasync(contextName, showContents, warnError);
+  }
+    
+  function warnError() {
+		// needs a more appropriate warning			    
+		alert('There was a problem with the request.');
+  }
+  
+  function modifyContext(context) {
+    return context;
+  }
+  
+  function renderTemplate() {
+      const templateName = "power_rankings.hb";
+      getJSONasync(templateName, loadTemplate, warnError);
+  }
+  window.addEventListener("load", renderTemplate);
+
 }
-const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
-toggleSwitch.addEventListener('change', switchTheme, false);
---
+++ power_rankings.hb
@@ -0,0 +1,22 @@
+
+<header>
+  <div class="header_img"></div>
+  <nav class="navbar">
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li class="active"><a href="news.html">News</a></li>
+      <li><a href="events.html">Events</a></li>
+      <li><a href="hall_of_fame.html">Hall of Fame</a></li>
+      <li><a href="about.html">About</a></li>
+      <li><a href="contact_us.html">Contact us</a></li>
+    </ul>
+  </nav>
+</header>
+
+<div class="main_content">
+	<h1>Power Rankings</h1>
+	<ul class="pr">
+		{{#each Smashtag}}
+		<li>{{this}}</li>
+	</ul>
+</div>
