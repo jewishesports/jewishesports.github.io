bc6969817124db42a2cf7425fd00db6031e0ca5b
Gavi Baron <barongavriel@gmail.com>
2020-06-26 16:06:48 -0400

    js 0.4

--- nhei.js
+++ nhei.js
@@ -1,5 +1,25 @@
 {
+  function readJSON(path) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', path, true);
+    xhr.onload = function(e) { 
+      if (this.status == 200) {
+          var file = new File([this.response], 'temp');
+          var fileReader = new FileReader();
+          fileReader.addEventListener('load', function(){
+               //do stuff with fileReader.result
+          });
+          fileReader.readAsText(file);
+      } 
+    }
+    xhr.send();
+  }
+
+
+
+
   function addPrList(){
+    var file = JSON.parse(readJSON("power_rankings.json"));
     for(int i = 0; i < 20; i++) {
       var list = document.createElement("li");
       var text = document.createTextNode(power_rankings["Melee PR"][i]["Smashtag"]);
